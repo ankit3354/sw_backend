4f39ae98ef59945aa0bfb7011ac3bc08efffaafb
Ankit Vishwakarma <ankitvishwakarma3354@gmail.com>
2026-05-11 15:53:26 +0530

    URL correction fix

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ const connectDB = require("./config/db");
 const app = express();
 
 /* Middleware */
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sw-technologies-three.vercel.app/",
+    ],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
